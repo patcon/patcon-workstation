5eeee7d9b762f71759ba21f3afeb8c6b5fc65b33
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-03-27 13:17:29 -0400

    Updated custom android sdk bits.

--- cookbook-stubs/patcon_android-sdk/attributes/default.rb
+++ cookbook-stubs/patcon_android-sdk/attributes/default.rb
@@ -1,1 +1,3 @@
 default['java']['jdk_version'] = '7'
+
+default['android-sdk']['components'] = %w{ build-tools-22.0.1 }
--- cookbook-stubs/patcon_android-sdk/recipes/default.rb
+++ cookbook-stubs/patcon_android-sdk/recipes/default.rb
@@ -1,5 +1,10 @@
 include_recipe 'android-sdk::default'
 
+# Needed to run aapt build-tool
+package 'gcc-multilib'
+package 'lib32z1'
+package 'lib32stdc++6'
+
 android_home = "/home/#{node['workstation']['user']}/android"
 
 directory android_home do
