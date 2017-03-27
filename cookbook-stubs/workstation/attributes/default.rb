64fbe72fd71efad25195004d6fde8679c04e50a6
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-03-27 14:35:24 -0400

    Overrode default janus packages.

--- cookbook-stubs/workstation/attributes/default.rb
+++ cookbook-stubs/workstation/attributes/default.rb
@@ -1,2 +1,4 @@
 default['workstation']['hub']['version'] = '2.2.1'
 default['workstation']['user'] = 'patcon'
+
+override['janus']['packages'] = %w{ ack-grep curl exuberant-ctags git-core libopenssl-ruby1.9.1 rake ruby-dev ruby vim vim-nox }
